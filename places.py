4b91cfe42a03825ecb69a989e716eccfd50b1d3b
Moulya <moulya.anjanappa@gmail.com>
2023-12-11 10:30:42 +0530

    updated places file

--- places.py
+++ places.py
@@ -1,71 +1,29 @@
-import tkinter as tk
-from tkinter import ttk
+from tkinter import *
+from tkinter import messagebox
+
+import sqlite3
+
+root = Tk()
+root.title('Login')
+root.geometry('925x500+300+200')
+root.configure(bg='#fff')
+root.resizable(False, False)
+
+img = PhotoImage(file='images/agra.png')
+Label(root, image=img, border=0, bg='white').place(x=50, y=90)
+Button(root, width=39, pady=7, text='Sign in',
+       bg='#57a1f8', fg='white', border=0).place(x=35, y=300)
 
 
-class TravelApp:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Travel and Tourism Management System")
+def query():
 
-        # Sample data for places to visit
-        self.places_data = [
-            {"name": "Paris", "description": "City of Love", "rating": 5},
-            {"name": "Tokyo", "description": "Vibrant Metropolis", "rating": 4},
-            {"name": "New York", "description": "The Big Apple", "rating": 4.5},
-            # Add more places as needed
-        ]
+    conn = sqlite3.connect('adress_book.bd')
 
-        # Create and set up the GUI
-        self.create_widgets()
+    c = conn.cursor()
+    c.execute("SELECT*,oid FROM addresses")
 
-    def create_widgets(self):
-        # Label to display the title
-        title_label = tk.Label(
-            self.root, text="Places to Visit", font=("Helvetica", 16, "bold"))
-        title_label.pack(pady=10)
-
-        # Treeview widget to display places data
-        columns = ("Name", "Description", "Rating")
-        self.tree = ttk.Treeview(
-            self.root, columns=columns, show="headings", height=10)
-
-        # Set column headings
-        for col in columns:
-            self.tree.heading(col, text=col)
-            self.tree.column(col, width=100, anchor="center")
-
-        # Insert data into the treeview
-        for place in self.places_data:
-            self.tree.insert("", "end", values=(
-                place["name"], place["description"], place["rating"]))
-
-        # Pack the treeview
-        self.tree.pack(padx=20, pady=10)
-
-        # Button to view more details
-        view_button = tk.Button(
-            self.root, text="View Details", command=self.view_details)
-        view_button.pack(pady=10)
-
-    def view_details(self):
-        # Get the selected item from the treeview
-        selected_item = self.tree.selection()
-
-        if not selected_item:
-            tk.messagebox.showwarning(
-                "No Selection", "Please select a place to view details.")
-            return
-
-        # Retrieve data from the selected item
-        item = self.tree.item(selected_item, "values")
-        name, description, rating = item[0], item[1], item[2]
-
-        # Display a messagebox with details
-        details_message = f"Name: {name}\nDescription: {description}\nRating: {rating}"
-        tk.messagebox.showinfo("Place Details", details_message)
+    conn.commit()
+    conn.close()
 
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = TravelApp(root)
-    root.mainloop()
+root.mainloop()
